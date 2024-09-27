6f725fd7b9be245ca2c5bf69c23ee27bf0d09399
Mr_maderator_YT <66310151+MrmaderatorYT@users.noreply.github.com>
2024-09-27 13:40:58 +0300

    Update notmain.c
    Додані команди help, echo <text>, debug on, debug off

--- notmain.c
+++ notmain.c
@@ -4,6 +4,8 @@
 #define UART0_DR (UART0_BASE + 0x00)
 #define UART0_FR (UART0_BASE + 0x18)
 
+typedef unsigned long size_t;
+
 void uart_putc(char c){
 	volatile uint32_t *UARTFR = (uint32_t *)UART0_FR;
 	volatile uint32_t *UARTDR = (uint32_t *)UART0_DR;
@@ -28,18 +30,79 @@ void uart_puts(const char *str){
 	}
 }
 
+int my_strcmp(const char *s1, const char *s2){
+	while (*s1 && (*s1 == *s2)){
+		s1++;
+		s2++;
+	}
+	return (unsigned char)*s1 - (unsigned char)*s2;
+}
+int my_strncmp(const char *s1, const char *s2, size_t n){
+	while (n > 0){
+		if(*s1 != *s2){
+			return (unsigned char)*s1 - (unsigned char)*s2;
+		}
+		if (*s1 == '\0'){
+			return 0;
+		}
+		s1++;
+		s2++;
+		n--;
+	}
+	return 0;
+}
+void process_command(const char *cmd, int *debug_mode){
+	uart_puts("\r\n");
+	
+	if(my_strcmp(cmd, "help") == 0){
+		uart_puts("Avaible commands: help, echo <message>, debug on, debug off");
+	}else if(my_strncmp(cmd, "echo ", 5) == 0){
+		uart_puts(cmd + 5);
+		uart_putc('\r');
+		uart_putc('\n');
+	}else if(my_strcmp(cmd, "debug on") == 0){
+		*debug_mode = 1;
+		uart_puts("Debug mode enabled. \r\n");
+	}else if(my_strcmp(cmd, "debug off") == 0){
+		*debug_mode = 0;
+		uart_puts("Debug mode disable. \r\n");
+	}else{
+		uart_puts("Unknown command. \r\n");
+	}
+}
+void print_command(const char *cmd, int debug_mode){
+	if(debug_mode){
+		uart_puts("\r\nReceived command: ");
+		uart_puts(cmd);
+		uart_putc('\r');
+		uart_putc('\n');
+	}
+}
 
 void notmain(void){
 	char c;
+	char command[100];
+	int index = 0;
+	int debug_mode = 0;//0 - debug off, 1 - on
 	uart_puts("UART test... \n");
 	while(1){
 		c = uart_getc();
 		if(c == '\r'){
-			uart_putc('\n');
+			command[index] = '\0';
+			print_command(command, debug_mode);
+			process_command(command, &debug_mode);
+			index = 0;
 		} else if (c == '\b' || c == 127){
-			uart_puts("\b \b");
+			if(index > 0){
+				index--;
+				uart_puts("\b \b");
+			}
 		} else{
-			uart_putc(c);
+			if(index < sizeof(command)-1){
+				command[index++] = c;
+				uart_putc(c);
+			}
 		}
 	}
 }
+
