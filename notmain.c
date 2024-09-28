365aa1bd14ec5c38533f1beeafd1305a0f903959
Mr_maderator_YT <66310151+MrmaderatorYT@users.noreply.github.com>
2024-09-28 14:03:47 +0300

    Update notmain.c
    робота з ОЗУ. Типу створили "файлову систему"

--- notmain.c
+++ notmain.c
@@ -1,10 +1,23 @@
-#include <stdint.h>
-
 #define UART0_BASE 0x09000000
 #define UART0_DR (UART0_BASE + 0x00)
 #define UART0_FR (UART0_BASE + 0x18)
 
+#define MAX_FILES 16
+#define MAX_NAME_LEGTH 32
+
 typedef unsigned long size_t;
+typedef unsigned int uint32_t;
+
+typedef struct{
+	char name [MAX_NAME_LEGTH];
+	int is_directory;
+} File;
+typedef struct{
+	File files[MAX_FILES];
+	int file_count;
+} FileSystem;
+
+FileSystem fs;
 
 void uart_putc(char c){
 	volatile uint32_t *UARTFR = (uint32_t *)UART0_FR;
@@ -51,11 +64,53 @@ int my_strncmp(const char *s1, const char *s2, size_t n){
 	}
 	return 0;
 }
+void my_strcpy(char *dest, const char *src){
+	while(*src){
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+}
+void init_fs(){
+	fs.file_count = 0;
+}
+int create_directory(const char *name){
+	if(fs.file_count >= MAX_FILES){
+		uart_puts("FileSystem is full, cannot create directory.\r\n");
+		return -1;
+	}
+	for (int i = 0; i < fs.file_count; i++){
+		if(my_strcmp(fs.files[i].name, name) == 0){
+			uart_puts("Directory already exist.\r\n");
+			return -1;
+		}
+	}
+	my_strcpy(fs.files[fs.file_count].name, name);
+	fs.files[fs.file_count].is_directory = 1;
+	fs.file_count++;
+	uart_puts("Directory created. \r\n");
+	return 0;
+}
+void list_directory(){
+	uart_puts("Directories:\r\n");
+	if(fs.file_count == 0){
+		uart_puts("No directories.\r\n");
+	}else{
+		for (int i = 0; i < fs.file_count; i++){
+			if(fs.files[i].is_directory){
+				uart_puts(fs.files[i].name);
+				uart_putc('\r');
+				uart_putc('\n');
+			}
+		}
+	}
+}
+	
+			
 void process_command(const char *cmd, int *debug_mode){
 	uart_puts("\r\n");
 	
 	if(my_strcmp(cmd, "help") == 0){
-		uart_puts("Avaible commands: help, echo <message>, debug on, debug off");
+		uart_puts("Avaible commands: help, echo <message>, debug on, debug off, mkdir <name>, ls. \r\n");
 	}else if(my_strncmp(cmd, "echo ", 5) == 0){
 		uart_puts(cmd + 5);
 		uart_putc('\r');
@@ -66,6 +121,10 @@ void process_command(const char *cmd, int *debug_mode){
 	}else if(my_strcmp(cmd, "debug off") == 0){
 		*debug_mode = 0;
 		uart_puts("Debug mode disable. \r\n");
+	}else if(my_strncmp(cmd, "mkdir ", 6) == 0){
+		create_directory(cmd + 6);
+	}else if (my_strcmp(cmd, "ls") == 0){
+		list_directory();
 	}else{
 		uart_puts("Unknown command. \r\n");
 	}
@@ -85,6 +144,7 @@ void notmain(void){
 	int index = 0;
 	int debug_mode = 0;//0 - debug off, 1 - on
 	uart_puts("UART test... \n");
+	init_fs();
 	while(1){
 		c = uart_getc();
 		if(c == '\r'){
@@ -105,4 +165,3 @@ void notmain(void){
 		}
 	}
 }
-
